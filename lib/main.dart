ad3ec29fa56e53ff1c86e381765c70b2c28a1d6c
Sadia Bennthe <tanzibamouri00@gmail.com>
2024-08-24 15:45:59 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:phone_state/phone_state.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Example(),
+    );
+  }
+}
+
+class Example extends StatefulWidget {
+  const Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  PhoneState status = PhoneState.nothing();
+  bool granted = false;
+
+  Future<bool> requestPermission() async {
+    var status = await Permission.phone.request();
+
+    return switch (status) {
+      PermissionStatus.denied ||
+      PermissionStatus.restricted ||
+      PermissionStatus.limited ||
+      PermissionStatus.permanentlyDenied =>
+      false,
+      PermissionStatus.provisional || PermissionStatus.granted => true,
+    };
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isIOS) setStream();
+  }
+
+  void setStream() {
+    PhoneState.stream.listen((event) {
+      setState(() {
+        status = event;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Phone State'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (Platform.isAndroid)
+              MaterialButton(
+                onPressed: !granted
+                    ? () async {
+                  bool temp = await requestPermission();
+                  setState(() {
+                    granted = temp;
+                    if (granted) {
+                      setStream();
+                    }
+                  });
+                }
+                    : null,
+                child: const Text('Request permission of Phone'),
+              ),
+            const Text(
+              'Status of call',
+              style: TextStyle(fontSize: 24),
+            ),
+            if (status.status == PhoneStateStatus.CALL_INCOMING ||
+                status.status == PhoneStateStatus.CALL_STARTED)
+              Text(
+                'Number: ${status.number}',
+                style: const TextStyle(fontSize: 24),
+              ),
+            Icon(
+              getIcons(),
+              color: getColor(),
+              size: 80,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  IconData getIcons() {
+    return switch (status.status) {
+      PhoneStateStatus.NOTHING => Icons.clear,
+      PhoneStateStatus.CALL_INCOMING => Icons.add_call,
+      PhoneStateStatus.CALL_STARTED => Icons.call,
+      PhoneStateStatus.CALL_ENDED => Icons.call_end,
+    };
+  }
+
+  Color getColor() {
+    return switch (status.status) {
+      PhoneStateStatus.NOTHING || PhoneStateStatus.CALL_ENDED => Colors.red,
+      PhoneStateStatus.CALL_INCOMING => Colors.green,
+      PhoneStateStatus.CALL_STARTED => Colors.orange,
+    };
+  }
+}
